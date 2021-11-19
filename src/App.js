231222b7205b0cb25241824b11d7d2ab01562641
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-11-19 18:59:35 -0500

    added routes to about.js project.js skills.js and contact.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NavBar from "./components/Navbar/Navbar";
 import Home from "./components/Home";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 
 function App() {
@@ -12,6 +16,18 @@ function App() {
         <Switch>
           <Route exact path="/">
           <Home />
+          </Route>
+           <Route exact path="/skills">
+         <Skills />
+          </Route>
+           <Route exact path="/projects">
+         <Projects />
+          </Route>
+           <Route exact path="/about">
+        <About />
+          </Route>
+           <Route exact path="/contact">
+         <Contact />
           </Route>
         </Switch>
       </div>
