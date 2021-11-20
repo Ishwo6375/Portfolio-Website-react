456647162851d612e2ba866759b388f995f85284
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-11-20 15:21:45 -0500

    added content to about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,68 @@
-import React from 'react'
+import React from "react";
+import about from "./styles/about.jpg";
+import "./styles/About.css";
 
 function About() {
-    return (
-        <div>
-            <h1>This is about page</h1>
+  return (
+    <div className="about-intro" id="about">
+      <h1 className="heading">
+        About <span>Me</span>
+      </h1>
+      <div className="row">
+        <div className="image">
+          <img src={about} alt="about-section-img" />
         </div>
-    )
+        <div className="content">
+          <h3>
+            Hello!! My name is <span>Ishwor Prajapati</span>
+          </h3>
+          <p className="info">
+            "Don't count the days, make the days count." - Muhammad Ali
+            <br />
+            Heloo! I'm Ishwor Prajapti, Fullstack webdeveloper based on
+            Baltimore, USA. I'm very passionate and dedicated to my work.I love
+            spending time on fixing little details and optimizing web apps. Also
+            I like working in a team. As the saying goes: "Two heads are better
+            than one".
+            <br />
+            Currently looking for an entry level position.I can't wait to find a
+            company where I can make my contribution. Feel free to message me or
+            email me at ishaan.prazapati6375@gmail.com. I'm always happy to
+            talk! :) Thank you Happy coding :)
+          </p>
+          <div className="box-container">
+            <div className="box">
+              <p>
+                <span>age: </span>28
+              </p>
+              <p>
+                <span>Gender: </span>Male
+              </p>
+              <p>
+                <span>Experience </span>Entry Level{" "}
+              </p>
+              <p>
+                <span>Freelance </span>available
+              </p>
+            </div>
+
+            <div className="box">
+              <p>
+                <span>Language: </span>English | Nepali | Korean | Nepal Bhasa| Hindi
+              </p>
+              <p>
+                <span>Email: </span>ishaan.prazapti6375@gmail.com
+              </p>
+              <p>
+                <span>Address </span>Baltimore, USA
+              </p>
+            </div>
+          </div>
+          <a href="#" className="btn">Download Resume</a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default About
+export default About;
